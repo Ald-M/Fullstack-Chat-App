f500607c2cd509ad5e5a120ba1a5a579b4fbcb21
Ald-M <aldo.m2705@gmail.com>
2025-08-30 22:00:49 +0700

    fix again

--- backend/src/index.js
+++ backend/src/index.js
@@ -54,7 +54,7 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   // IMPORTANT: Express 5 doesn't like "*" anymore
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
